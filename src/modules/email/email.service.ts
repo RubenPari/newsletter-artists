d8fd9508175f8e934cb8f82e10c6b9549f086202
RubenPari <rubenpari.developer@gmail.com>
2022-06-05 08:49:51 +0200

    mapped email service

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sgMail = require('@sendgrid/mail');
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  constructor() {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  }
+
+  async sendEmail(to: string, subject: string, text: string, html: string) {
+    const msg = {
+      to: to,
+      from: process.env.SENDGRID_FROM,
+      subject: subject,
+      text: text,
+      html: html,
+    };
+    await sgMail.send(msg);
+  }
+}
